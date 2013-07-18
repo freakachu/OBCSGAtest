4e4030070fec4662cfe500ba652dd0aa6fece43d
jlambright <joshua.lambright@gmail.com>
2013-07-17 22:49:59 -0400

    Google API Integration Imports
    These imports should allow us to make our own FusionTables API client, quite easily.

--- sgtest.py
+++ sgtest.py
@@ -3,6 +3,27 @@ import random
 
 import webapp2
 import jinja2
+import httplib2
+import logging
+import pickle
+import datetime
+
+"""
+The following imports are for OAuth2 & Google API interation. This shoud allow us to use the built-in method found in:
+  
+  Fusion Tables: https://google-api-client-libraries.appspot.com/documentation/fusiontables/v1/python/lates/index.html
+  
+  OAuth2Client: https://google-api-python-client.googlecode.com/hg/docs/epy/oauth2client-module.html
+
+"""
+
+from apiclient.discovery import build
+from oauth2client.appengine import AppAssertionCredentials
+from oauth2client.client import SignedJwtAssertionCredentials
+from google.appengine.api import memcache
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
 
 
 
